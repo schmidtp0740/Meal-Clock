89be3cfe49cd51fb82e776762d7c7473aa247060
schmidtp0740 <schmidtp0740@gmail.com>
2016-06-20 19:54:57 -0700

    Created Source.cpp and entered credentials and libraries

---
+++ SnakeEyes/Source.cpp
@@ -0,0 +1,18 @@
+/*
+Project Name: Snake Eyes
+Author: Phiip Schmidt
+Date: June 20, 2016
+Description:
+
+
+
+*/
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+int main()
+{
+
+	return 0;
+}
