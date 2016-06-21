9f2a4ff38d5f54cf17821249cfd8b1152d5c0cc1
schmidtp0740 <schmidtp0740@gmail.com>
2016-06-20 20:13:47 -0700

    Completed Project. Working. Next step is to create a Die Class

--- SnakeEyes/Source.cpp
+++ SnakeEyes/Source.cpp
@@ -9,10 +9,28 @@ Description:
 */
 #include <iostream>
 #include <iomanip>
+#include <time.h>
+#include <windows.h>
 
 using namespace std;
 int main()
 {
+	srand(time(NULL));
+	int die[2];
+	for (int i = 0; i < 100; i++)
+	{
+		die[0] = rand() % 6 + 1;
+		die[1] = rand() % 6 + 1;
+		cout << "Roll " << i << ": " << die[0] << " " << die[1];
 
+		//if dies equal 1 then print snake eyes;
+		if (die[0] == 1 && die[1] == 1)
+		{
+			cout << " Snake Eyes!!";
+		}
+		cout << endl;
+		Sleep(50);
+	}
+	system("pause");
 	return 0;
 }
